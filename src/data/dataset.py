de4f07d2868fe5a1999041651a787ef6e36082cc
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-26 17:09:19 +0700

    “Update”

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -7,4 +7,8 @@ def get_dataset():
     dataset_dir = "./datasets/20NG"
     download_dataset("20NG", cache_path="./datasets")
     dataset = topmost.data.BasicDataset(dataset_dir, read_labels=True, device=DEVICE, contextual_embed=True, doc_embed_model='all-MiniLM-L6-v2')
+    print("train_size:", len(dataset.train_data) if hasattr(dataset, "train_data") else "N/A")
+    print("test_size:", len(dataset.test_data) if hasattr(dataset, "test_data") else "N/A")
+    print("vocab_size:", dataset.vocab_size if hasattr(dataset, "vocab_size") else "N/A")
+    print("average length:", dataset.avg_len if hasattr(dataset, "avg_len") else "N/A")
     return dataset
