71101eeba309c0dbfc049b0531716fb849d90b3d
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-26 17:11:52 +0700

    “Update”

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -6,7 +6,7 @@ from src.config.config import DEVICE
 def get_dataset():
     dataset_dir = "./datasets/20NG"
     download_dataset("20NG", cache_path="./datasets")
-    dataset = topmost.data.BasicDataset(dataset_dir, read_labels=True, device=DEVICE, contextual_embed=True, doc_embed_model='all-MiniLM-L6-v2')
+    dataset = topmost.data.BasicDataset(dataset_dir, read_labels=True, device=DEVICE, contextual_embed=False)
     print("train_size:", len(dataset.train_data) if hasattr(dataset, "train_data") else "N/A")
     print("test_size:", len(dataset.test_data) if hasattr(dataset, "test_data") else "N/A")
     print("vocab_size:", dataset.vocab_size if hasattr(dataset, "vocab_size") else "N/A")
