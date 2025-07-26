7d87638b81082a842836547623eead63342d25c8
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-26 16:56:14 +0700

    “Update”

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,10 @@
+import topmost
 from topmost import download_dataset
+from src.config.config import DEVICE
 
 
 def get_dataset():
     dataset_dir = "./datasets/20NG"
     download_dataset("20NG", cache_path="./datasets")
+    dataset = topmost.data.BasicDataset(dataset_dir, read_labels=True, device=DEVICE, pretrained_WE=True)
+    return dataset
